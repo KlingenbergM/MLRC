c18def0a6075f292430d8372bfdc7a7e0e4853bb
KlingenbergM <155055909+KlingenbergM@users.noreply.github.com>
2025-07-29 21:07:56 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,8 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-
-# adjust imports if database/models are in app/ folder
-from database import engine, SessionLocal
-from models import Base, User
-
+from fastapi import FastAPI
 from app.auth import router as auth_router
 from app.leaderboard import router as leaderboard_router
 
 app = FastAPI()
-
-# Create DB tables
-Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
@@ -19,15 +10,3 @@ def root():
 
 app.include_router(auth_router)
 app.include_router(leaderboard_router)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.get("/users")
-def get_users(db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    return [{"strava_id": u.strava_id, "access_token": u.access_token} for u in users]
