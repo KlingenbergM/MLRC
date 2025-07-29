48fdc084350da6d33d0b8deed02c3fe2fb694fcc
KlingenbergM <155055909+KlingenbergM@users.noreply.github.com>
2025-07-29 21:45:44 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.leaderboard import router as leaderboard_router
 from database import engine
 from models import Base
 from app.segment_test import router as segment_test_router
-app.include_router(segment_test_router)
+include_router(segment_test_router)
 
 
 Base.metadata.create_all(bind=engine)
