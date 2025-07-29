f319c4280fccf22bef06d6898506691ec44ce9f0
KlingenbergM <155055909+KlingenbergM@users.noreply.github.com>
2025-07-29 21:16:06 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from app.auth import router as auth_router
 from app.leaderboard import router as leaderboard_router
+from app.database import engine
+from app.models import Base
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
